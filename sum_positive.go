061c1774a93e61a4d1248668bf280a85864bbb82
Edson Celio <edsoncelio@users.noreply.github.com>
2018-10-10 16:17:56 -0300

    Update sum_positive.go

--- sum_positive.go
+++ sum_positive.go
@@ -5,6 +5,8 @@ func PositiveSum(numbers []int) int {
    for _, value := range numbers{
      if value > 0 { 
        sum =  sum + value
+     }else{
+         sum = sum - value
      }
    } 
    return sum
